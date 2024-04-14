768e0258ac11f8ef779579c497006bb8eaf5e630
sam23287 <dhoni23287@gmail.com>
2024-04-14 17:35:05 +0530

    first commit

---
+++ f1.py
@@ -0,0 +1,5 @@
+a=10
+b=10
+c=a+b
+print(c)
+print('press i for "insert" and then esc to escape and :wq for exiting the code editor and go to the command line')
---
+++ f2.py
@@ -0,0 +1,2 @@
+learnbay classes
+learnbay classes
